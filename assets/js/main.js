cceda5894b859b76b7ce36b4291b7a2c8e053437
Gavin <gmd3659@rit.edu>
2020-04-21 22:16:08 -0400

    Face added and other things added

--- assets/js/main.js
+++ assets/js/main.js
@@ -62,12 +62,9 @@ jQuery(document).ready(function($) {
         }
     );
     
-    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
-    
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "gmd3659", selector: "#ghfeed" });
 
 
 });
